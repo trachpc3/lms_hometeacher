f0a58572a2f10b88660b1d002d568542818f6f7e
trachpc3 <73776243+trachpc3@users.noreply.github.com>
2025-08-21 08:59:55 +0200

    Update config.js

--- lms-frontend/src/config.js
+++ lms-frontend/src/config.js
@@ -1,8 +1,8 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://86.109.171.91:3001/api";
+  import.meta.env.VITE_API_BASE_URL || "http://86.109.171.91:5000/api";
 
 export const API_UPLOADS_URL =
-  import.meta.env.VITE_API_UPLOADS_URL || "http://86.109.171.91:3001";
+  import.meta.env.VITE_API_UPLOADS_URL || "http://86.109.171.91:5000/uploads";
 
 export const GOOGLE_CLIENT_ID =
   import.meta.env.VITE_GOOGLE_CLIENT_ID || "";

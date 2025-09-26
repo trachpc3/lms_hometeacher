2f7a91ba8964a01fda8b117c3e13ee886f051bac
trachpc3 <73776243+trachpc3@users.noreply.github.com>
2025-09-26 14:38:34 +0200

    Update levelsData.js

--- lms-frontend/src/data/levelsData.js
+++ lms-frontend/src/data/levelsData.js
@@ -1,8 +1,9 @@
+// src/data/levelsData.js
 export const levels = [
-    { id: "Beginners", name: "Beginners", unlocked: true },
-    { id: "Lower", name: "Lower Intermediate", unlocked: false },
-    { id: "Intermediate", name: "Intermediate", unlocked: false },
-    { id: "Upper", name: "Upper Intermediate", unlocked: false },
-    { id: "Advanced", name: "Advanced", unlocked: false },
-    { id: "Business", name: "Business", unlocked: false },
+  { id: "beginners", name: "Beginners" },
+  { id: "lower-intermediate", name: "Lower Intermediate" },
+  { id: "intermediate", name: "Intermediate" },
+  { id: "upper-intermediate", name: "Upper Intermediate" },
+  { id: "advanced", name: "Advanced" },
+  { id: "business", name: "Business" },
 ];
